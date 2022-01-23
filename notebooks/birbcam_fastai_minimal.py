ffce334e4119c27f9562b3571cdddd7319f09767
Max Watermolen <mwateremolen@maxwelldps.com>
2022-01-23 15:30:43 -0600

    prep for train

--- notebooks/birbcam_fastai_minimal.py
+++ notebooks/birbcam_fastai_minimal.py
@@ -2,15 +2,15 @@ from fastai.vision.all import *
 import pandas as pd
 
 # Table of labels: columns "fname" and "labels"
-df = pd.read_csv("labels.csv")
-path = Path("birbcam/data/")
+df = pd.read_csv("inital_training_data.csv")
+path = Path("data/")
 
 # Create our data loader for training and validation, with automatic image
 # resizing, cropping, and data augmentation (flips, rotation, skew, etc.).
 dls = ImageDataLoaders.from_df(df, path, folder='training', label_delim=',',
                                item_tfms=Resize(224), 
                                batch_tfms=aug_transforms(size=224))
-
+dls.show_batch()
 # Create our model, with pretrained ResNet 34 weights
 learn = cnn_learner(dls, resnet34, metrics=partial(accuracy_multi, thresh=0.5))
 
